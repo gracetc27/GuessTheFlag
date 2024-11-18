edc6ec5f2798e4e967b0939f0f40a8f529062d89
Grace Couch <gracetcouch@gmail.com>
2024-11-18 16:50:10 +0000

    adds accessibility labels for flag buttons

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -11,6 +11,20 @@ struct ContentView: View {
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
     @State private var correctAnswers = Int.random(in: 1...2)
 
+    let labels = [
+        "Estonia": "Flag with three horizontal stripes. Top stripe blue, middle stripe black, bottom stripe white.",
+        "France": "Flag with three vertical stripes. Left stripe blue, middle stripe white, right stripe red.",
+        "Germany": "Flag with three horizontal stripes. Top stripe black, middle stripe red, bottom stripe gold.",
+        "Ireland": "Flag with three vertical stripes. Left stripe green, middle stripe white, right stripe orange.",
+        "Italy": "Flag with three vertical stripes. Left stripe green, middle stripe white, right stripe red.",
+        "Nigeria": "Flag with three vertical stripes. Left stripe green, middle stripe white, right stripe green.",
+        "Poland": "Flag with two horizontal stripes. Top stripe white, bottom stripe red.",
+        "Spain": "Flag with three horizontal stripes. Top thin stripe red, middle thick stripe gold with a crest on the left, bottom thin stripe red.",
+        "UK": "Flag with overlapping red and white crosses, both straight and diagonally, on a blue background.",
+        "Ukraine": "Flag with two horizontal stripes. Top stripe blue, bottom stripe yellow.",
+        "US": "Flag with many red and white stripes, with white stars on a blue background in the top-left corner."
+    ]
+
     @State private var showingScore = false
     @State private var scoreTitle = ""
     @State private var userScore = 0
@@ -43,6 +57,7 @@ struct ContentView: View {
                                 .shadow(radius: 10)
                                 .clipShape(.rect(cornerRadius: 8))
                         }
+                        .accessibilityLabel(labels[countries[number], default: "Unknown flag"])
                     }
                 }
                 .frame(maxWidth: .infinity)
